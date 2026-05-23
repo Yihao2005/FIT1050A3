502cb5b0e62324d529b09b274bacd344127298f1
yihaoyu <yyuu0140@student.monash.edu>
2026-05-24 00:27:33 +1000

    Feat: Correct the typo in html and js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -3,7 +3,7 @@ const contactForm = document.querySelector("#contact-form");
 contactForm.addEventListener("submit",function(event){
     event.preventDefault();
 
-    alert("Submitted Sucessfully! We will contact you ASAP!");
+    alert("Submitted Successfully! We will contact you ASAP!");
 
     contactForm.reset();
 })
